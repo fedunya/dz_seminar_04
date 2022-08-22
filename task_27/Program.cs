f8c7abc251db0a21bc073a3d119267af93ddf22e
Fedor <farlep1970@yandex.ru>
2022-08-22 22:27:06 +0300

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+long SumDigits(long num)
+{
+    long result = 0;
+    while (num != 0)
+    {
+        long digit = num % 10;
+        result = result + digit;
+        num = num / 10;
+    }
+    return result;
+}
+
+for(;;)
+{
+    Console.Write("Введите целое число (Для выхода введите Y): ");
+    var numberString = Console.ReadLine()!;
+    if(numberString.Equals("Y"))
+        break;
+    if (long.TryParse(numberString, out var number))
+    {
+        number = Math.Abs(number);
+        long sum = SumDigits(number);
+        Console.WriteLine("Сумма цифр в числе равна: " + sum);
+    }
+    else
+    {
+        Console.WriteLine("Введено не число!");
+    }
+}
