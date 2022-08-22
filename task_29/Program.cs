eb3d52a9de0a42f40cfb8461d1b8fe0a27a03ab2
Fedor <farlep1970@yandex.ru>
2022-08-22 22:54:56 +0300

    task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+void PrintArray(int[] array)
+{
+    int lenght = array.Length;
+    Console.Write($"[{array[1]}");
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+int[] CreateArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.Write($"Введите {i+1}-е число массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+var arr = CreateArray(8);
+Console.WriteLine();
+PrintArray(arr);
